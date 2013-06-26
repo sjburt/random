11e261ee18e8fc84081062fb065bdd6b595035f9
sjburt <vheissu@gmail.com>
2013-06-25 22:32:55 -0700

    no actual progress

--- fecfun/fecencode.c
+++ fecfun/fecencode.c
@@ -3,9 +3,6 @@
 #include <string.h>
 #include "fecencode.h"
 
-#define Q (3)
-#define eps (.02)
-
 typedef struct fecencoder {
   uint32_t blocks_generated;
   uint8_t * data;
@@ -13,9 +10,14 @@ typedef struct fecencoder {
   uint32_t length;
 } fecencoder;
 
+static void
+gen_block_list(const int seqnum, uint8_t* count, uint16_t blocklist[]);
+
+
+
 fecRef_t fenc_create(uint8_t* data, uint32_t len, uint16_t block_size){
-  fecRef_t newFec;
-  newFec = malloc(sizeof(fecencoder));
+  fecRef_t newFec = NULL;
+  newFec = (fecRef_t)malloc(sizeof(fecencoder));
 
   newFec->data = data;
   newFec->length = len;
