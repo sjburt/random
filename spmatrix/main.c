d90b7ef9f143ae1788c942a037fb5a84273f5358
sjburt <vheissu@gmail.com>
2013-06-30 23:55:37 -0700

    spmatrix: more makefile work. working better now. not perfect yet

--- spmatrix/main.c
+++ spmatrix/main.c
@@ -7,5 +7,6 @@ int main(int argv, char**argc) {
     printf("%s ", argc[i]);
   }
   printf("\n");
+  a_test_function(20);
   return 0;
 }
--- spmatrix/spmatrix_test.cpp
+++ spmatrix/spmatrix_test.cpp
@@ -1,7 +1,13 @@
-#define BOOST_TEST_MODULE const_string test
-#include <boost/test/unit_test.hpp>
 #include <iostream>
 extern "C" {
 #include "spmatrix.h"
 }
+#define BOOST_TEST_MODULE MyTest
+#include <boost/test/unit_test.hpp>
 
+
+BOOST_AUTO_TEST_CASE( my_test) {
+
+  BOOST_CHECK_EQUAL(0, a_test_function(10));
+
+}
